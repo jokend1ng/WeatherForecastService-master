5ed61e2a7c5e527f47df1ef61793017f839e2d2a
jokend1ng <75688359+jokend1ng@users.noreply.github.com>
2024-04-09 20:49:08 +0300

    Update MapperConfig.java

--- src/main/java/com/example/configuration/MapperConfig.java
+++ src/main/java/com/example/configuration/MapperConfig.java
@@ -3,7 +3,9 @@ package com.example.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+/**
+    В контексте создаются бин ObjectMapper
+*/    
 @Configuration
 public class MapperConfig {
     @Bean
